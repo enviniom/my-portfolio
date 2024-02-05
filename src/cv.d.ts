aac864ca436649942efcd832b4918d58a7232ded
Jhon Colmenares <jhonccr@hotmail.com>
2024-02-04 21:32:56 -0500

    implñemented internationalization

--- src/cv.d.ts
+++ src/cv.d.ts
@@ -1,113 +1,136 @@
-
 export interface CV {
-  basics: Basics
-  work: Array<Work>
-  volunter: Array<Volunter>
-  education: Array<Education>
-  awards: Array<Awards>
-  certificates: Array<Certificates>
-  publications: Array<Publications>
-  skills: Array<Skills>
-  languages: Array<Languages>
-  interests: Array<Interests>
-  references: Array<References>
-  projects: Array<Projects>
+  basics: Basics;
+  work: Array<Work>;
+  volunter: Array<Volunter>;
+  education: Array<Education>;
+  awards: Array<Awards>;
+  certificates: Array<Certificates>;
+  publications: Array<Publications>;
+  skills: Array<Skills>;
+  languages: Array<Languages>;
+  interests: Array<Interests>;
+  references: Array<References>;
+  projects: Array<Projects>;
 }
 
 interface Basics {
-  name: string
-  label: string
-  image: string
-  email: string
-  phone: string
-  url: string
-  summary: string
-  location: Location
-  profiles: Array<Profiles>
+  name: string;
+  label: string;
+  image: string;
+  email: string;
+  phone: string;
+  url: string;
+  summary: string;
+  location: Location;
+  profiles: Array<Profile>;
 }
 
 interface Location {
-  addres: string
-  postalCode: string
-  city: string
-  countryCode: string
-  region: string
+  addres: string;
+  postalCode: string;
+  city: string;
+  countryCode: string;
+  region: string;
 }
 
-interface Profiles {
-  network: string
-  username: string
-  url: string
+interface Profile {
+  network: string;
+  username: string;
+  url: string;
 }
 
 interface Work {
-  name: string
-  position: string
-  url: string
-  startDate: DateStr
-  endDate: DateStr | null
-  summary: string
-  highlights: Highlight
+  title: string;
+  items: {
+    name: string;
+    position: string;
+    url: string;
+    startDate: DateStr;
+    endDate: DateStr | null;
+    summary: string;
+    highlights: Highlight;
+  }[];
 }
 
-type DateStr = `${string}-${string}-${string}`
+type DateStr = `${string}-${string}-${string}`;
 
 interface Volunter {
-  organization: string
-  position: string
-  url: string
-  startDate: DateStr
-  endDate: DateStr
-  summary: string
-  highlights: Highlight
+  title: string;
+  items: {
+    organization: string;
+    position: string;
+    url: string;
+    startDate: DateStr;
+    endDate: DateStr;
+    summary: string;
+    highlights: Highlight;
+  }[];
 }
 
 interface Skills {
-  name: string
-  level: string
-  keywords: Array<string>
+  title: string;
+  items: {
+    name: string;
+    level: string;
+    keywords: Array<string>;
+  }[];
 }
 
 interface Awards {
-  title: string
-  date: string
-  awarder: string
-  summary: string
+  title: string;
+  items: {
+    title: string;
+    date: string;
+    awarder: string;
+    summary: string;
+  }[];
 }
 
 interface Certificates {
-  name: string,
-  date: DateStr,
-  issuer: string,
-  url: string
+  title: string;
+  items: {
+    name: string;
+    date: DateStr;
+    issuer: string;
+    url: string;
+  }[];
 }
 
 interface Publications {
-  name: string
-  publisher: string
-  releaseDate: DateStr
-  url: string
-  summary: string
+  title: string;
+  items: {
+    name: string;
+    publisher: string;
+    releaseDate: DateStr;
+    url: string;
+    summary: string;
+  }[];
 }
 
 interface Education {
-  institution: string
-  url: string
-  area: sring
-  studyType: string
-  startDate: DateStr
-  endDate: DateStr
-  score: string
-  courses: Array<string>
+  title: string;
+  items: {
+    institution: string;
+    url: string;
+    area: sring;
+    studyType: string;
+    startDate: DateStr;
+    endDate: DateStr;
+    score: string;
+    courses: Array<string>;
+  }[];
 }
 
 interface Languages {
-  language: Language
-  frequency: string
+  title: string;
+  items: {
+    language: Language;
+    frequency: string;
+  }[];
 }
 
 type Language =
-  "Spanish"
+  | "Spanish"
   | "English"
   | "German"
   | "France"
@@ -122,25 +145,34 @@ type Language =
   | "Turkish"
   | "Hindi"
   | "Bengali"
-  | string
+  | string;
 
 interface Projects {
-  name: string
-  isActive: boolean
-  description: string
-  highlights: Highlight
-  url: string
-  github?: string
+  title: string;
+  items: {
+    name: string;
+    isActive: boolean;
+    description: string;
+    highlights: Highlight;
+    url: string;
+    github?: string;
+  }[];
 }
 
 interface Interests {
-  name: string
-  keywords: Array<stirng>
+  title: string;
+  items: {
+    name: string;
+    keywords: Array<stirng>;
+  }[];
 }
 
 interface References {
-  name: string
-  reference: string
+  title: string;
+  items: {
+    name: string;
+    reference: string;
+  }[];
 }
 
-type Highlight = Array<String>
+type Highlight = Array<String>;
