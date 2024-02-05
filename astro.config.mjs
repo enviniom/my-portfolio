1e6c586e16e84a949543599373b5e55dc80becd8
Jhon Colmenares <jhonccr@hotmail.com>
2024-02-05 07:48:47 -0500

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jhonccr.github.io',
+  base: '/my-portfolio',
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
