f56b410e265b76a6fd9edf56d0f346b94892f6c8
Jhon Colmenares <jhonccr@hotmail.com>
2025-06-29 12:46:47 -0500

    update astro deploy in github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jhonccr.github.io',
+  site: 'https://enviniom.github.io',
   base: '/my-portfolio',
   i18n: {
     defaultLocale: "en",
